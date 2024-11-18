4198bf954962fc553f183b5c73b7ec010a5a7c10
rairai77 <cutenitki@gmail.com>
2024-11-18 11:55:45 -0800

    added JMP instruction

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from typing import List
 from enum import Enum, auto
 
 class I(Enum): # Enum for more convenient access, changes and reverse lookup built in
-    VAL = auto()
-    MEM = auto()
-    MOV = auto()
+    VAL = auto() # HANDLED
+    MEM = auto() # HANDLED
+    MOV = auto() # HANDLED
     ADD = auto()
     SUB = auto()
     MUL = auto()
@@ -72,7 +72,17 @@ class CPU:
         return self._memory[self._ip - 1]
     
     def r(self, idx):
-        return self._registers[idx.value]
+        return self._registers[idx.value-1]
+
+    def load_src(self):
+        src = self.next_instruction()
+        if (src == I.VAL):
+            src = self.next_instruction()
+        elif (src == I.MEM):
+            src = self._memory[self.next_instruction()]
+        else:
+            src = self.r(src)()
+        return src
 
     def exec(self):
         self._ip = 0 
@@ -81,32 +91,33 @@ class CPU:
             instruction = self.next_instruction()
             match instruction:
                 case I.MOV:
-                    src = self.next_instruction()
-                    if (src == I.VAL):
-                        src = self.next_instruction()
-                    elif (src == I.MEM):
-                        src = self._memory[self.next_instruction()]
-                    else:
-                        src = self.r(src)
+                    src = self.load_src()
                     dest = self.next_instruction()
                     if (dest == I.MEM):
                         self._memory[self.next_instruction()] = src
                     else:
                         self.r(dest)(src)
+                case I.JMP:
+                    self._ip = self.load_src()
                 case I.STOP:
                     stopped = True
 
 if __name__ == "__main__":
     memory = [
-        I.MOV,
-        I.VAL,
-        37,
-        R.G2,
+        I.MOV, I.VAL, 37, R.G2,
+        I.MOV, R.G2, R.G1,
+        I.MOV, R.G1, I.MEM, 30,
+        I.MOV, I.MEM, 30, R.G3,
+        I.JMP, I.VAL, 19,
         I.STOP,
-    ] + [0]*32
+        I.MOV, R.G3, R.G4,
+        I.STOP
+    ] + [0]*200
     c = CPU(memory)
     c.exec()
-    print(c._registers[R.G2.value]())
+    for idx, register in enumerate(c._registers):
+        print(R(idx+1), register())
+    print(c._memory)
 
 
 
